73c11c3f1cc1e1d3260d4ef23b29263e97de9a08
DiegoCamilli <camillidiego@yahoo.com>
2023-05-15 19:01:11 -0700

    Cleande up code to start writing our app.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,27 +1,44 @@
-const { Tech, Matchup } = require('../models');
+const { AuthenticationError } = require('apollo-server-express')
+const { Profile } = require('../models')
+const { signToken } = require('../utils/auth')
 
 const resolvers = {
   Query: {
-    tech: async () => {
-      return Tech.find({});
+    profiles: async () => {
+      return Profile.find()
     },
-    matchups: async (parent, { _id }) => {
-      const params = _id ? { _id } : {};
-      return Matchup.find(params);
+
+    profile: async (parent, { profileId }) => {
+      return Profile.findOne({ _id: profileId })
     },
   },
+
   Mutation: {
-    createMatchup: async (parent, args) => {
-      const matchup = await Matchup.create(args);
-      return matchup;
+    addProfile: async (parent, { name, email, password }) => {
+      const profile = await Profile.create({ name, email, password })
+      const token = signToken(profile)
+
+      return { token, profile }
     },
-    createVote: async (parent, { _id, techNum }) => {
-      const vote = await Matchup.findOneAndUpdate(
-        { _id },
-        { $inc: { [`tech${techNum}_votes`]: 1 } },
-        { new: true }
-      );
-      return vote;
+    login: async (parent, { email, password }) => {
+      const profile = await Profile.findOne({ email })
+
+      if (!profile) {
+        throw new AuthenticationError('No profile with this email found!')
+      }
+
+      const correctPw = await profile.isCorrectPassword(password)
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password!')
+      }
+
+      const token = signToken(profile)
+      return { token, profile }
+    },
+
+    removeProfile: async (parent, { profileId }) => {
+      return Profile.findOneAndDelete({ _id: profileId })
     },
   },
 };
