1c845671de7c1edf0c8393dcb6b30dc20688d5b0
magic404 <jamesbox3@gmail.com>
2023-05-16 17:42:37 -0700

    Published Out Code MongoDb-Completed-JWTSSTARTED-GRAPHQL

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
-
+const foodSchema = require('./Food');
 const profileSchema = new Schema({
   name: {
     type: String,
@@ -19,13 +19,17 @@ const profileSchema = new Schema({
     required: true,
     minlength: 5,
   },
-  age: [
-    {
+  age: {
       type: Number,
       trim: true,
     },
-  ],
+    savedFoods:[foodSchema]
+},{
+  toJSON: {
+    virtuals: true,
+  },
 });
+
 
 profileSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
@@ -39,7 +43,14 @@ profileSchema.pre('save', async function (next) {
 profileSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
-
+profileSchema.virtual('totalCalories').get(function () {
+  //return this.savedFoods.length; 
+  let totalCalories=0 
+  for (let i = 0; i < this.savedFoods.length; i++) { 
+    totalCalories+=this.savedFoods[i].calories
+  }
+  return totalCalories
+} );
 const Profile = model('Profile', profileSchema);
 
 module.exports = Profile;
